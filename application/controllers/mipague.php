ca4be7c3d78e862f0f820d3cda1dccb59c54bc81
Gabriel Oliveira <gabriel.oliveira@evolucard.com.br>
2012-04-11 01:37:32 -0300

    primeiro commit

---
+++ application/controllers/mipague.php
@@ -0,0 +1,9 @@
+<?php
+
+class Mipague extends CI_Controller {
+	function index() {
+		$this->load->view('mipague_view');
+	}
+}
+
+?>
---
+++ application/views/mipague_view.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>miPague</title>
+</head>
+<body>
+<h1>miPague</h1>
+</body>
+</html>
